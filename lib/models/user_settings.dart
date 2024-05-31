f417d8895fde81b577bf8027dd9ab32bbc480697
CantarinoG <guilhermecantarino1709@gmail.com>
2024-05-31 20:03:39 -0300

    model: Add UserSettings model.

---
+++ lib/models/user_settings.dart
@@ -0,0 +1,13 @@
+class UserSettings {
+  final double calorieGoal;
+  final double carbGoal;
+  final double proteinGoal;
+  final double fatGoal;
+
+  UserSettings(
+    this.calorieGoal,
+    this.carbGoal,
+    this.proteinGoal,
+    this.fatGoal,
+  );
+}
